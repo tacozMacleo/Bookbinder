ad41ec51bdfba6a0c9892be02f7a9b22b9bd0e6f
Michael Bach Kristensen <tacoz666@gmail.com>
2023-02-26 16:40:59 +0100

    Main - Added.

---
+++ main.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import argparse
+import io
+import logging
+import pathlib
+
+from itertools import islice
+
+from pypdf import PdfReader
+from pypdf import PdfWriter
+from pypdf import PaperSize
+
+from pdfnup import generateNup
+from pdfnup import _mtA4Pdf
+
+
+the_empty_page = io.BytesIO(_mtA4Pdf)
+
+
+def batched(iterable, n: int):
+    "Batch data into lists of length n. The last batch may be shorter."
+    "Coming in Python 3.12, without pading"
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be >= 1')
+    it = iter(iterable)
+    while (batch := list(islice(it, n))):
+        yield batch
+
+
+def empty_page_fill(pages: int, section_pages: int) -> int:
+    page_count_per_section = section_pages * 4
+    empty_pages_add = (
+        page_count_per_section - (pages % page_count_per_section)
+    )
+    # if empty_pages_add % page_count_per_section == 0:
+    #     return 0
+    return empty_pages_add
+
+
+def generate_page_order(
+    page_count: int,
+    section_count: int
+) -> list[int]:
+    r_list = []
+    for each_section in batched(range(page_count), section_count*4):
+        while each_section:
+            r_list.append(each_section.pop())
+            r_list.append(each_section.pop(0))
+    return r_list
+
+
+def add_empty_pages_to_fit_section_count(
+    input_file: pathlib.Path,
+    section_count: int
+) -> tuple[io.BytesIO, int]:
+    with input_file.open('rb') as file:
+        pdf_reader = PdfReader(file)
+        page_count: int = len(pdf_reader.pages)
+
+        empty_page_count = empty_page_fill(page_count, section_count)
+        pdf_writer = PdfWriter()
+        pdf_writer.append_pages_from_reader(pdf_reader)
+        for _ in range(empty_page_count):
+            pdf_writer.append(the_empty_page)
+
+        page_w_empty_pages = io.BytesIO()
+
+        pdf_writer.write(page_w_empty_pages)
+        pdf_writer.close()
+        page_w_empty_pages.seek(0)
+        return page_w_empty_pages, page_count + empty_page_count
+
+
+def order_pages(file: io.BytesIO, the_order: list[int]) -> io.BytesIO:
+    merger = PdfWriter()
+    merger.append(file, pages=the_order)
+    page_ordered = io.BytesIO()
+    merger.write(page_ordered)
+    merger.close()
+    page_ordered.seek(0)
+    return page_ordered
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='bookbinder',
+        description='A Bookbinding helper program.',
+        # epilog = 'Text at the bottom of help'
+    )
+
+    parser.add_argument(
+        'src',
+        type=pathlib.Path,
+        help="The source PDF file from which the section corrected file are make.",
+    )
+
+    parser.add_argument(
+        'out',
+        type=pathlib.Path,
+        help="The section corrected file.",
+    )
+
+    parser.add_argument(
+        '-s', '--section',
+        type=int,
+        help="The page Count for a section."
+    )
+
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Enable Debugging info.',
+    )
+
+    # parser.add_argument(
+    #     '-f', '--folds',
+    #     type=int,
+    #     help="The count of fold per page.",
+    # )
+    folds: int = 2
+
+    args = parser.parse_args()
+
+    page_w_empty_pages, page_count = add_empty_pages_to_fit_section_count(
+        input_file=args.src,
+        section_count=args.section,
+    )
+
+    the_order = generate_page_order(
+        page_count=page_count,
+        section_count=args.section,
+    )
+
+    ordered_file = order_pages(
+        file=page_w_empty_pages,
+        the_order=the_order,
+    )
+
+    generateNup(ordered_file, folds, str(args.out))
